41b6b9d6ffa38c4cb88b8679553e8835dad8b2d8
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2023-07-22 00:24:20 +0600

    upload

---
+++ agency-nextapp/components/TeamDetails.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "@/styles/teamdetails.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+function TeamDetails({title, avatar}) {
+  const img = "";
+  return (
+    <>
+      {/* page design starting */}
+      <div className={styles.page}>
+          {/* <Image
+            className={styles.img}
+            src="/srk.jpg"
+            width={200}
+            height={200}
+            alt="Picture of the author"
+          /> */}
+          <Image
+            className={styles.img}
+            // src="/srk.jpg"
+            src = {avatar}
+            width={200}
+            height={200}
+            alt="Picture of Team member"
+          />
+        {/* <h1>{"Tech Expert's"}</h1> */}
+        <h1>{title}</h1>
+        <Link className={styles.link} href="/">Home</Link>
+      </div>
+    </>
+  );
+}
+
+export default TeamDetails;
---
+++ agency-nextapp/pages/team.js
@@ -0,0 +1,21 @@
+import TeamDetails from '@/components/TeamDetails'
+import React from 'react'
+import styles from "@/styles/team.module.css";
+
+function team() {
+
+  const avatar = '/srk.jpg';
+  const description = 'Gregorio Y. Zara';
+  const title = 'Tech Expert'
+  const title1 = 'SEO Expert'
+
+  return (
+    <div className={styles.page}>
+    <TeamDetails title={title}  avatar={avatar} />
+    
+    <TeamDetails title={title1}  avatar={avatar} />
+    </div>
+  )
+}
+
+export default team
